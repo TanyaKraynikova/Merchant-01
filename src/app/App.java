7a64e109de300564202a2a9ee78fe4144622c78f
Tanya <krajnikova2004@gmail.com>
2022-04-12 00:29:35 +0300

    Add class properties

--- src/app/App.java
+++ src/app/App.java
@@ -1,6 +1,17 @@
 package app;
 
+// Класс-входная точка в приложение.
+// App launcher.
 public class App {
+
+    static String merchantName;
+    static String email;
+    static String phone;
+    static String productName;
+    static int quantity;
+    static double price;
+
+
     public static void main(String[] args) {
 
     }
